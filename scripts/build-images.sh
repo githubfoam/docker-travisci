288967938bfb77971b5f92c91a3145d533090a97
githubfoam <lequestionis@gmail.com>
2020-06-26 21:56:35 +0300

    прох`ожего

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -5,26 +5,34 @@ set -o nounset
 set -o xtrace
 # set -eox pipefail #safety for script
 
+# export DOCKERDIR="dockerfiles"
+# export DOCKERFILE="Dockerfile.alpine.fio"
+# export DOCKER_IMAGE="fio"
+# export DOCKERTAG="alpine"
+# export DOCKER_REPO="testrepo"
+
 export DOCKERDIR="dockerfiles"
 export DOCKERFILE="Dockerfile.alpine.fio"
-export DOCKER_IMAGE="fio"
-export DOCKERTAG="alpine"
-export DOCKER_REPO="testrepo"
+export DOCKER_IMAGE="alpine"
+export DOCKER_REPO="fio"
 
 echo "=============================build============================================================="
 docker build -t $DOCKER_IMAGE:$TRAVIS_COMMIT . --file $DOCKERDIR/$DOCKERFILE
 docker images ls
 echo "=============================push============================================================="
 echo $DOCKER_TOKEN | sudo docker login --username $DOCKER_USERNAME --password-stdin #Login Succeeded
+
 export git_sha="${TRAVIS_COMMIT}"
-docker tag $DOCKER_IMAGE:$TRAVIS_COMMIT "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
-docker push "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
-docker run -p 8000:80 "$DOCKER_USERNAME/$DOCKER_REPO:$DOCKER_IMAGE" &
+# docker tag "$DOCKER_IMAGE:$TRAVIS_COMMIT" "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+# docker push "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+docker tag "$DOCKER_IMAGE:$TRAVIS_COMMIT" "$DOCKER_USERNAME/$DOCKER_REPO:$DOCKER_IMAGE"
+docker push "$DOCKER_USERNAME/$DOCKER_REPO:$DOCKER_IMAGE"
 docker image ls
-docker container ls
+
+
 sudo docker logout
-echo "=============================pull && smoke tests============================================================="
-docker image rm --force "$DOCKER_IMAGE:$TRAVIS_COMMIT"
-docker image rm --force "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
-docker image ls
-docker pull "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+# echo "=============================pull && smoke tests============================================================="
+# docker image rm --force "$DOCKER_IMAGE:$TRAVIS_COMMIT"
+# docker image rm --force "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+# docker pull "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+# docker image ls
