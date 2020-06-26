8094afa73c892d989bc0db5a36367497ab24a5b3
githubfoam <lequestionis@gmail.com>
2020-06-26 21:27:43 +0300

    прох`ожий

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # docker-travisci
+
+
  [![Build Status](https://travis-ci.com/githubfoam/docker-travisci.svg?branch=dev)](https://travis-ci.com/githubfoam/docker-travisci)  
 
-~~~~
 
+~~~~
+VDBench
+
+Download VDBench. The current official version is available from the Oracle Technology Network (OTN)
+An older version remains on SourceForge
+https://sourceforge.net/projects/vdbench/
+http://wiki.lustre.org/VDBench
+
+fio
+https://fio.readthedocs.io/en/latest/index.html
 ~~~~
--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -6,29 +6,25 @@ set -o xtrace
 # set -eox pipefail #safety for script
 
 export DOCKERDIR="dockerfiles"
-export DOCKERFILE="Dockerfile.ansible"
-export DOCKER_IMAGE="testimage"
-export DOCKERTAG="ubuntu1804:ansible"
+export DOCKERFILE="Dockerfile.alpine.fio"
+export DOCKER_IMAGE="fio"
+export DOCKERTAG="alpine"
 export DOCKER_REPO="testrepo"
 
 echo "=============================build============================================================="
-docker images ls
-# docker build --build-arg OS_IMAGE=centos:8 -t centos8:ping-v1 . --file $DOCKERDIR/custom.dockerfile
-# docker build --build-arg OS_IMAGE=ubuntu:16.04 -t ubuntu1604:ping-v1 . --file $DOCKERDIR/custom.dockerfile
-# docker run -it centos8:ping-v1 -c 2 www.google.com
-# docker build -t fabric2:bionic . --file $DOCKERDIR/$DOCKERDIR
-# sudo docker build -t fabric2:bionic . --file $DOCKERDIR/Dockerfile.fabric2
 docker build -t $DOCKER_IMAGE:$TRAVIS_COMMIT . --file $DOCKERDIR/$DOCKERFILE
 docker images ls
 echo "=============================push============================================================="
-
 echo $DOCKER_TOKEN | sudo docker login --username $DOCKER_USERNAME --password-stdin #Login Succeeded
-
 export git_sha="${TRAVIS_COMMIT}"
-
 docker tag $DOCKER_IMAGE:$TRAVIS_COMMIT "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
 docker push "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
 docker run -p 8000:80 "$DOCKER_USERNAME/$DOCKER_REPO:$DOCKER_IMAGE" &
 docker image ls
 docker container ls
 sudo docker logout
+echo "=============================pull && smoke tests============================================================="
+docker image rm --force "$DOCKER_IMAGE:$TRAVIS_COMMIT"
+docker image rm --force "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
+docker image ls
+docker pull "$DOCKER_USERNAME/$DOCKER_REPO:${git_sha}-${TRAVIS_BRANCH}"
