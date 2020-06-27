22c2c629acb8a066e4610dc84a8e6cdd3be46737
githubfoam <lequestionis@gmail.com>
2020-06-27 13:06:03 +0300

    остановл`ю

--- scripts/deploy-microservices.sh
+++ scripts/deploy-microservices.sh
@@ -6,21 +6,43 @@ set -o xtrace
 # set -eox pipefail #safety for script
 
 
-echo "=============================Microservices demo with event driven architecture============================================================="
-#https://github.com/cealer/microservices-demo
-git clone https://github.com/cealer/microservices-demo.git && cd microservices-demo
-docker-compose build #Build the services
-docker-compose ps
-docker-compose up -d #Run services
-sleep 1m
-curl http://localhost:61945/Records
-curl http://127.0.0.1:61945/Records
-curl http://localhost:8888 #Open Angular client
-curl http://127.0.0.1:8888
+# echo "=============================Microservices  with event driven architecture============================================================="
+# #https://github.com/cealer/microservices-demo
+# git clone https://github.com/cealer/microservices-demo.git && cd microservices-demo
+# docker-compose build #Build the services
+# docker-compose ps
+# docker-compose up -d #Run services
+# sleep 1m
+# curl http://localhost:61945/Records
+# curl http://127.0.0.1:61945/Records
+# curl http://localhost:8888 #Open Angular client
+# curl http://127.0.0.1:8888
+#
+# docker-compose down #Stop and remove the services
 
-docker-compose down #Stop and remove the services
+echo "=============================Microservices  with event driven architecture============================================================="
+#https://github.com/djarza/football-events
+git clone https://github.com/djarza/football-events.git && cd football-events
 
-#this node is not a swarm manager. Use "docker swarm init" or "docker swarm join" to connect this node to swarm and try again
-# docker stack deploy  -c docker-compose.prod.yml --with-registry-auth salary-app #deployment
-# docker stack services salary-app #Check that it’s running
-# docker stack rm salary-app
+cat /etc/hosts
+cat <<EOT | sudo tee -a /etc/hosts
+127.0.0.1 kafka
+127.0.0.1 postgres
+127.0.0.1 connect
+127.0.0.1 football-match
+127.0.0.1 football-player
+127.0.0.1 football-view-basic
+127.0.0.1 football-view-top
+127.0.0.1 football-ui
+EOT
+cat /etc/hosts
+
+cd football && mvn install #Build microservices and Docker containers
+
+#Build and run the demo application:
+cd football-tests & mvn install -DskipTests
+java -jar target/football-tests-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+# java -jar target/football-tests-0.0.1-SNAPSHOT-jar-with-dependencies.jar 400 #If the default startup timeout (180 sec) will be insufficient, try to increase
+sleep 2m
+
+curl http://football-ui:18080/
